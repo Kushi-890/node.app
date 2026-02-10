cb20d26628a53957a9a8d13d85cc78d9ce105d10
Kushi-890 <kushijeej@gmail.com>
2026-02-10 23:14:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! This is my first Node.js project for DevOps.');
+  res.end('hi,my name is kushi and im using java');
 });
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
+
