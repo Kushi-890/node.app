89c1cc9789c50d52f2df2878fc150a48de6c5097
Kushi-890 <kushijeej@gmail.com>
2026-02-11 16:39:09 +0530

    added Dockerfile, Jenkinsfile, README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node DevOps App
+Simple Node.js app for DevOps practice.
+node index.js
+3000
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! This is my first Node.js project for DevOps.');
+  res.end('hi,my name is kushi and im using java');
 });
 
 server.listen(3000, () => {
